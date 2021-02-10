f5389fcef49f8233282ba28e79dcb602caa8e2fd
noah <nxb9626@rit.edu>
2021-02-09 19:47:34 -0500

    Updates

---
+++ precache-manifest.7752247dd2618860ec3c6ceb183b4c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afb96c317ada960acd5b7d0ba3e1e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a67c3079fc5c8d83cc0",
+    "url": "/static/css/main.d6117677.chunk.css"
+  },
+  {
+    "revision": "5ae6d135e4ed4b8c0952",
+    "url": "/static/js/2.841fcab1.chunk.js"
+  },
+  {
+    "revision": "0a67c3079fc5c8d83cc0",
+    "url": "/static/js/main.81a0a08c.chunk.js"
+  },
+  {
+    "revision": "24970a687e0f2a146320",
+    "url": "/static/js/runtime~main.cb024171.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.9b1159fe4bb5b8db072ec9a0a972b907.js"
+  "/precache-manifest.7752247dd2618860ec3c6ceb183b4c96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
